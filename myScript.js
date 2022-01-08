d85acb732ef4366901308e458c790af8c43683fc
Edward Shen <edwardshen24@gmail.com>
2022-01-07 23:25:28 -0800

    .

--- myScript.js
+++ myScript.js
@@ -1,7 +1,6 @@
 
 const computerSelection = computerPlay();
 
-
 function computerPlay(){
     let choices = ["Rock","Paper","Scissors"];
     let computerChoice = choices[Math.floor(Math.random()*3)];
@@ -9,22 +8,23 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection) {
-    let revisedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let revisedPlayerSelection = playerSelection.charAt(0).toUpperCase() 
+        + playerSelection.slice(1).toLowerCase();
     
     if (revisedPlayerSelection === computerSelection)
-        return ("It's a tie.");
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, it's a tie.`);
     else if(revisedPlayerSelection === "Rock" && computerSelection === "Paper")
-        return ("You lose.");
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you lose.`);
     else if(revisedPlayerSelection === "Rock" && computerSelection === "Scissors")
-        return ("You win.");
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you win.`);
     else if(revisedPlayerSelection === "Paper" && computerSelection === "Rock")
-        return ("You win.");
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you win.`);
     else if(revisedPlayerSelection === "Paper" && computerSelection === "Scissors")
-        return ("You lose.");
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you lose.`);
     else if(revisedPlayerSelection === "Scissors" && computerSelection === "Rock")
-        return ("You lose.")
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you lose.`);
     else if(revisedPlayerSelection === "Scissors" && computerSelection === "Paper")
-        return ("You win.")
+        return (`You chose ${revisedPlayerSelection} and the computer chose ${computerSelection}, you win.`);
   }
 
  
@@ -32,7 +32,7 @@ function game(){
     let playerSelection;
     for (let i = 1; i<=5; i++){
         playerSelection = prompt("Enter your choice:");
-        alert( playRound(playerSelection, computerPlay()));
+        console.log( playRound(playerSelection, computerPlay()));
     }
 }
 
